b9fbf6317f32de375244d38508abb05e2efa8616
Pedro Miguel Bandeira Franco <pmbfr@vestas.com>
2021-07-15 09:59:49 +0100

    adding new dependency to check worflow behaviour

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
+using Vestas.TurbineSwRollout.Common.RetryPolicy.Models;
+
 namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var e = new ErrorModel
+            {
+                Category = ErrorCategory.Exception
+            };
+
+            Console.WriteLine($"Hello World! Category={e.Category}");
         }
     }
 }
