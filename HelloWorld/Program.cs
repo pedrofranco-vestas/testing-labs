cd89cee3a3af5f4a4251df710eae178add61dba4
Pedro Miguel Bandeira Franco <pmbfr@vestas.com>
2021-07-15 15:10:32 +0100

    removing test dependency

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,19 +1,12 @@
 ﻿using System;
 
-using Vestas.TurbineSwRollout.Common.RetryPolicy.Models;
-
 namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var e = new ErrorModel
-            {
-                Category = ErrorCategory.Exception
-            };
-
-            Console.WriteLine($"Hello World! Category={e.Category}");
+            Console.WriteLine("Hello World!");
         }
     }
 }
