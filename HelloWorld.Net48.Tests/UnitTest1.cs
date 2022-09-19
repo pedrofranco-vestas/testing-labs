a173b2f6ece0638a505d1b2032ec8dc5fdd75e05
Pedro Miguel Bandeira Franco <pmbfr@vestas.com>
2022-09-19 16:27:09 +0100

    adding new project and workflow

---
+++ HelloWorld.Net48.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace HelloWorld.Net48.Tests
+{
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Console.WriteLine("hello world, this is not really a test");
+        }
+    }
+}
